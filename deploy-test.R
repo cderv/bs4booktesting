87bf08e6b01a2d3f3cd8f7b1301c74a8fe20ab61
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-06 16:09:24 +0200

    deploy new book test

--- deploy-test.R
+++ deploy-test.R
@@ -1,6 +1,6 @@
 # This file is used to generated the different test book during PRs
 repo <- "~/DEV_R/bookdown"
-base_url <- "https://cderv.github.io/bs4booktesting/"
+base_url <- "https://cderv.github.io/bs4booktesting"
 
 # 404 testing
 gert::git_branch_checkout("maelle-404", repo = repo)
@@ -13,6 +13,16 @@ gert::git_push()
 # x-scroll
 out_dir <- "docs/wrap-and-scroll"
 gert::git_branch_checkout("maelle-overflow", repo = repo)
+devtools::load_all(repo)
+render_book(".", "bookdown::bs4_book", output_dir = out_dir, quiet = TRUE)
+gert::git_add(out_dir)
+gert::git_commit(sprintf("Rebuild book for %s", basename(out_dir)))
+gert::git_push()
+utils::browseURL(file.path(base_url, basename(out_dir)))
+
+# copy button on input only
+out_dir <- "docs/nocopybuttononoutput"
+gert::git_branch_checkout("bs4book/nocopybuttononoutput", repo = repo)
 devtools::load_all(repo)
 render_book(".", "bookdown::bs4_book", output_dir = out_dir, quiet = TRUE)
 gert::git_add(out_dir)

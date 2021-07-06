f278a6f1fbec53fd1b3dd04414baca8f74a9c886
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-06 20:41:38 +0200

    branch merged

--- deploy-test.R
+++ deploy-test.R
@@ -10,12 +10,3 @@ gert::git_add("docs/")
 gert::git_commit("Rebuild base book")
 gert::git_push()
 
-# x-scroll
-out_dir <- "docs/wrap-and-scroll"
-gert::git_branch_checkout("maelle-overflow", repo = repo)
-devtools::load_all(repo)
-render_book(".", "bookdown::bs4_book", output_dir = out_dir, quiet = TRUE)
-gert::git_add(out_dir)
-gert::git_commit(sprintf("Rebuild book for %s", basename(out_dir)))
-gert::git_push()
-utils::browseURL(file.path(base_url, basename(out_dir)))

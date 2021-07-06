1abb6f28bcee37d82627542896889b93cb1463c1
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-06 14:02:01 +0200

    branch for icon change is merged

--- deploy-test.R
+++ deploy-test.R
@@ -8,11 +8,3 @@ render_book(".", "bookdown::bs4_book", quiet = TRUE)
 gert::git_add("docs/")
 gert::git_commit("Rebuild base book")
 gert::git_push()
-
-# icon testing
-gert::git_branch_checkout("bs4book/change-copy-btn", repo = repo)
-devtools::load_all(repo)
-render_book(".", "bookdown::bs4_book", quiet = TRUE, output_dir = "docs/icon-copy-btn")
-gert::git_add("docs/icon-copy-btn/")
-gert::git_commit("Rebuild icon-copy-btn")
-gert::git_push()

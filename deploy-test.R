c250c63b13c000c8c0278580fe59204e750eae5b
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-13 22:26:14 +0200

    only 404 branch left

--- deploy-test.R
+++ deploy-test.R
@@ -7,11 +7,6 @@ gert::git_branch_checkout("master", repo = repo)
 devtools::load_all(repo)
 render_book(".", "bookdown::bs4_book", quiet = TRUE)
 
-# master branch
-gert::git_branch_checkout("fix/bs4book-encoding", repo = repo)
-devtools::load_all(repo)
-render_book(".", "bookdown::bs4_book", quiet = TRUE)
-
 # 404 testing
 gert::git_branch_checkout("maelle-404", repo = repo)
 devtools::load_all(repo)
@@ -20,17 +15,6 @@ gert::git_add("docs/")
 gert::git_commit("deploy base book")
 gert::git_push()
 utils::browseURL(base_url)
-
-# with_scroll
-gert::git_branch_checkout("unscroll-code-chunk", repo = repo)
-devtools::load_all(repo)
-render_book(".", "bookdown::bs4_book", quiet = TRUE, output_dir = "docs/with-scroll")
-servr::httd("docs/with-scroll")
-gert::git_add("docs/")
-gert::git_commit("deploy with-scroll version book")
-gert::git_push()
-utils::browseURL(file.path(base_url, "with-scroll/"))
-
 
 # gitbook
 devtools::load_all(repo)
